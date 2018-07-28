e100b5e3ab6c2933e00773898aa895cf4ebd8289
wraithjpr <james.p.r.wraith@gmail.com>
2018-07-28 23:40:59 +0100

    First tutorial scenario.

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,3 +1,23 @@
 const assert = require('assert');
 const { Given, When, Then } = require('cucumber');
 
+function isItFriday (today) {
+  return  'Friday' === today ? 'TGIF' : 'Nope';
+}
+
+Given('today is Sunday', function () {
+  this.today = 'Sunday';
+});
+
+Given('today is Friday', function () {
+  this.today = 'Friday';
+});
+
+When('I ask whether it\'s Friday yet', function () {
+  this.actualAnswer = isItFriday(this.today);
+});
+
+Then('I should be told {string}', function (expectedAnswer) {
+  assert.equal(this.actualAnswer, expectedAnswer);
+});
+
